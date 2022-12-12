d625e43d76098e35f22bc2d132a639c6aa535650
jl3579 <jl3579@cornell.edu>
2022-12-12 12:41:26 -0500

    change variable name correctly

---
+++ sector_total_jobs.js
@@ -0,0 +1,13 @@
+const sectorLineChart = d3.select("#linechart2");
+    const sectorWidth = sectorLineChart.attr("width");
+    const sectorHight = sectorLineChart.attr("height");
+    const sectorMargin = {top: 10, right: 10, bottom: 50, left: 50};
+    const chartWidth = sectorWidth - sectorMargin.left - sectorMargin.right;
+    const chartHeight = sectorHight - sectorMargin.top - sectorMargin.bottom;
+
+    let annotations = sectorLineChart.append("g").attr("id","annotations");
+    let chartArea = sectorLineChart.append("g").attr("id","points")
+                    .attr("transform",`translate(${sectorMargin.left},${sectorMargin.top})`);
+
+
+
