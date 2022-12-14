552ddce936b7e63ae92a5dcbea9fe622c8342a8b
yunqin <wyq0307qyw@gmail.com>
2022-12-14 01:46:04 -0500

    style

--- linechart.js
+++ linechart.js
@@ -1,6 +1,6 @@
 //basic linePLot setting
 
-const lineSvg = d3.select("svg#lineplot");
+const lineSvg = d3.select("svg#lineplot") ;
 const lineWidth = lineSvg.attr("width");
 const lineHeight = lineSvg.attr("height");
 const lineMargin = { top: 50, right: 10, bottom: 50, left: 50 };
@@ -8,10 +8,16 @@ const linePlotWidth = lineWidth - lineMargin.left - lineMargin.right;
 const linePlotHeight = lineHeight - lineMargin.top - lineMargin.bottom;
 
 const lineAnnotations = lineSvg.append("g").attr("id", "line-annotations");
-const lineChartArea = lineSvg
-  .append("g")
-  .attr("id", "chart")
-  .attr("transform", `translate(${lineMargin.left},${lineMargin.top})`);
+
+lineSvg.append("rect").attr("x",lineMargin.left).attr("y",lineMargin.top)
+                .attr("width",linePlotWidth)
+                .attr("height",linePlotHeight)
+                .attr("fill","#000")
+                .attr("opacity",0.05)
+
+const lineChartArea = lineSvg.append("g")
+                            .attr("id", "chart")
+                            .attr("transform", `translate(${lineMargin.left},${lineMargin.top})`);
 
 const loadLinePlotData = async function () {
   const linePlotData = await d3.csv("unemployedRate.csv", d3.autoType);
@@ -39,14 +45,13 @@ const loadLinePlotData = async function () {
   newData["Clear"] = newData.Overall;
 
   //x axis
-  //!!!!2021 didn't show
-  const lineYearExtent = d3.extent(sliceLine, (sliceLine) => sliceLine.year);
-
-  const lineYearScale = d3
-    .scaleTime()
-    .domain(lineYearExtent)
-    .range([0, linePlotWidth - 20]);
-  let lineBottomAxis = d3.axisBottom(lineYearScale);
+  let lineYearExtent = d3.extent(sliceLine, (sliceLine) => sliceLine.year);
+  const lineYearScale = d3.scaleTime()
+                          .domain(lineYearExtent)
+                          .range([20, linePlotWidth-20 ]);
+  let lineBottomAxis = d3.axisBottom(lineYearScale)
+                         .ticks(lineYearExtent[1].getFullYear()
+                                -lineYearExtent[0].getFullYear()+1);
 
   lineAnnotations
     .append("g")
@@ -77,39 +82,9 @@ const loadLinePlotData = async function () {
 
   //draw the line
 
-  //get key text
-  const buttonText = Object.keys(newData);
-  const index = buttonText.indexOf("Overall");
-  buttonText.splice(index, 1);
 
-  buttonText.forEach((d) => {
-    d3.select("div#line-button-bar")
-      .append("button")
-      .attr("class", "line-button")
-      .style("margin", "10px")
-      .text(d)
-      .on("click", function () {
-        updateLine(d);
-      });
-  });
 
-  //button style
-  let allButton = d3.selectAll(".line-button"); // select all elements with class "line-button"
-  const nodes = allButton.nodes();
-  allButton
-    .style("background", "none")
-    .style("border", "1px solid black")
-    .style("border-radius", "5px")
-    .style("padding", "3x 10px 5px 10px"); // apply styles to elements
-
-  allButton.on("mouseover", function (d, i) {
-    d3.select(this).style("background", "black");
-    d3.select(this).style("color", "white");
-  });
-  allButton.on("mouseout", function () {
-    d3.select(this).style("background", "none");
-    d3.select(this).style("color", "black");
-  });
+  
 
   //Active Ovrerall
   const active = newData.Overall;
@@ -181,7 +156,7 @@ const loadLinePlotData = async function () {
   }
 
   //legend:
-  const yValue = "percentage";
+  const yValue = "Percentage";
   const keys = ["Overall", "Men", "Women"];
 
   var legendColor = d3
@@ -208,22 +183,16 @@ const loadLinePlotData = async function () {
     .enter()
     .append("g")
     .attr("class", "lineLegend")
-    .attr("transform", function (d, i) {
-      return `translate(${linePlotWidth - 30},${i * 20 + 15})`;
-    });
+    .attr("transform", (d, i)=> `translate(${650+i*80},${15})`);
 
-  lineLegend
-    .append("text")
-    .text(function (d) {
-      return d;
-    })
-    .attr("transform", "translate(15, 6)"); //align texts with boxes
+  lineLegend.append("text")
+            .text(d=>d)
+            .attr("transform", "translate(20, 6)");
 
-  lineLegend
-    .append("rect")
-    .attr("fill", (d) => legendColor(d))
-    .attr("width", 12)
-    .attr("height", 5);
+  lineLegend.append("rect")
+            .attr("fill", d => legendColor(d))
+            .attr("width", 16)
+            .attr("height", 5);
 
   //marker lines
   const marker = function (value) {
@@ -231,9 +200,8 @@ const loadLinePlotData = async function () {
     let xMarker = mouseGroup
       .append("line")
       .attr("id", "xMarker")
-      .attr("fill", "none")
-      .attr("stroke", "black")
-      .attr("stroke-width", 1)
+      .attr("stroke", "#aaa")
+      .attr("stroke-width", 0.5)
       .attr("y1", 0)
       .attr("y2", linePlotHeight)
       .attr("visibility", "hidden");
@@ -242,16 +210,14 @@ const loadLinePlotData = async function () {
       .append("circle")
       .attr("id", "valueMarker")
       .attr("fill", "none")
-      .attr("stroke", "black")
-      .attr("fill", "black")
-      .attr("stroke-width", 2)
+      .attr("fill", "steelblue")
       .attr("r", 6)
       .attr("opacity", 0.5)
       .attr("visibility", "hidden");
 
     let label = mouseGroup
       .append("text")
-      .attr("id", "label")
+      .attr("id", "gender-label")
       .attr("visibility", "hidden");
 
     let activeRegion = mouseGroup
@@ -277,7 +243,6 @@ const loadLinePlotData = async function () {
     let findDate = d3.bisector((value) => value["year"]).right;
 
     activeRegion.on("mousemove", function (evt) {
-      // Update the position as you move
 
       // Get mouse location
       let location = d3.pointer(evt);
@@ -296,29 +261,44 @@ const loadLinePlotData = async function () {
       xMarker.attr("x1", xPos).attr("x2", xPos);
       valueMarker.attr("cx", xPos).attr("cy", yPos);
 
-      let txt =
-        `${value[index].year.getFullYear()}` +
-        ` ` +
-        `${value[index].rate + "%"}`;
+      let txt =`${value[index].year.getFullYear()}, ${value[index].rate + "%"}`;
 
-      // We started with the following line, which has lots of overlaps
-      // label.attr("x",xPos).attr("y",yPos).text(txt);
-
-      // Here's a simple way to remove overlaps and deliver a consistent user experience
       label.text(txt);
       if (xPos < linePlotWidth / 2.0) {
-        label
-          .attr("x", xPos + 4)
-          .attr("y", 100)
-          .attr("text-anchor", "start");
+        label.attr("x", xPos + 4)
+            .attr("y", 100)
+            .attr("text-anchor", "start");
       } else {
-        label
-          .attr("x", xPos - 4)
-          .attr("y", linePlotHeight - 100)
-          .attr("text-anchor", "end");
+        label.attr("x", xPos - 4)
+            .attr("y", linePlotHeight - 100)
+            .attr("text-anchor", "end");
       }
     });
   };
+    //get key text
+    const buttonText = Object.keys(newData);
+    const index = buttonText.indexOf("Overall");
+    buttonText.splice(index, 1);
+  
+    buttonText.forEach((d) => {
+      d3.select("div#line-button-bar")
+        .append("button")
+        .attr("class", "line-button")
+        .style("margin", "10px")
+        .text(d)
+        .on("mouseover",function(){
+          d3.select(this).style("background",myColor(d))
+          .style("color","#000")
+        })
+        .on("mouseout",function(){
+          d3.select(this).style("background","none")
+          .style("color","#000")
+        })
+        .on("click", function () {
+            updateLine(d);
+        });
+    });
+
   marker(active);
 };
 loadLinePlotData();
